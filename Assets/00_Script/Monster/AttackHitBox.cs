24f18e1863c20d4d2e3a962758854390e9dd8959
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-07-06 22:59:23 +0900

    Attack 애니메이션때에만 공격이 통하게

--- Assets/00_Script/Monster/AttackHitBox.cs
+++ Assets/00_Script/Monster/AttackHitBox.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class AttackHitBox : MonoBehaviour
 {
     public float damage = 20f;
+    Animator animator;
 
+    void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
     void OnTriggerEnter(Collider other)
     {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            return;
+
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerState>()?.TakeDamage(damage);
--- Assets/00_Script/Monster/Behaviours/AttackBehaviour.cs
+++ Assets/00_Script/Monster/Behaviours/AttackBehaviour.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class AttackBehaviour : StateMachineBehaviour
 {
+    public float hitTimeStart = 1.08f;
+    public float hitTimeEnd = 1.18f;
+
+    Transform hitBox;
+    bool isEnabled;
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var hitBox = animator.transform.Find("AttackHitBox");
-        if (hitBox) hitBox.gameObject.SetActive(true);
+        hitBox = animator.transform.Find("hand.r/AttackHitBox");
+        if (hitBox) hitBox.gameObject.SetActive(false);
+        isEnabled = false;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (hitBox == null) return;
+
+        float clipLength = stateInfo.length; 
+        float time = (stateInfo.normalizedTime % 1f) * clipLength;
+
+        if (!isEnabled && time >= hitTimeStart && time < hitTimeEnd)
+        {
+            hitBox.gameObject.SetActive(true);
+            isEnabled = true;
+        }
+        else if (isEnabled && time >= hitTimeEnd)
+        {
+            hitBox.gameObject.SetActive(false);
+        }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var hitBox = animator.transform.Find("AttackHitBox");
         if (hitBox) hitBox.gameObject.SetActive(false);
     }
 }
