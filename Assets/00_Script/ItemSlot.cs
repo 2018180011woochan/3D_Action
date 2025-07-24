7abdc52fbc762d43118c5511adc1514345a15b1c
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-07-24 14:51:24 +0900

    포션 사용

--- Assets/00_Script/ItemSlot.cs
+++ Assets/00_Script/ItemSlot.cs
@@ -1,26 +1,50 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     public Item curItem;
     public Image standardImage;
     public Image curSlotImage;
 
-    void Start()
+    private float lastClickTime = 0f;
+    private float doubleClickTime = 0.3f;
+
+/*    void Start()
     {
-        standardImage = curSlotImage;
-    }
+        standardImage.sprite = curSlotImage.sprite;
+    }*/
 
     public void AddItem(Item newItem)
     {
         curItem = newItem;
         curSlotImage.sprite = newItem.ItemImage;
+        curSlotImage.enabled = true;
     }
 
     public void ClearSlot()
     {
         curItem = null;
-        curSlotImage = standardImage;
+        curSlotImage.sprite = standardImage.sprite;
+        curSlotImage.enabled = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (curItem == null) return;
+
+        float clickTime = Time.time - lastClickTime;
+
+        if (clickTime <= doubleClickTime)
+        {
+            if (curItem.ItemName == "Potion")
+            {
+                Debug.Log("포션 아이템 사용");
+                ClearSlot();
+            }
+        }
+
+        lastClickTime = Time.time;
     }
 }
