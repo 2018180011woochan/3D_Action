23b693ca97d38e743a6bb82d5a09e30ebbd937ac
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-08-07 21:21:16 +0900

    사무라이 공격 패턴 구현

--- Assets/00_Script/Player/Samurai/SamuraiCombat.cs
+++ Assets/00_Script/Player/Samurai/SamuraiCombat.cs
@@ -5,6 +5,10 @@ public class SamuraiCombat : MonoBehaviour
     private Animator animator;
     private SamuraiMovement movementScript;
 
+    public float doubleClickTime = 0.1f;
+    private bool isWaitingDoubleClick = false;
+    private float firstClickTime;
+
     void Awake()
     { 
         animator = GetComponent<Animator>();
@@ -14,20 +18,50 @@ public class SamuraiCombat : MonoBehaviour
     void Update()
     {
         if (movementScript.Stance == false) return;
-
         if (movementScript.isBusy) return;
+
+        if (isWaitingDoubleClick && Time.time - firstClickTime > doubleClickTime)
+        {
+            isWaitingDoubleClick = false;
+            Attack1();
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
-            Attack1();
+            if (!isWaitingDoubleClick)
+            {
+                isWaitingDoubleClick = true;
+                firstClickTime = Time.time;
+            }
+            else
+            {
+                Attack3();
+                isWaitingDoubleClick = false;
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Attack2();
         }
     }
+
 
     private void Attack1()
     {
         movementScript.isBusy = true;
-
         animator.SetTrigger("Attack1");
+    }
+
+    private void Attack2()
+    {
+        movementScript.isBusy = true;
+        animator.SetTrigger("Attack2");
+    }
+
+    private void Attack3()
+    {
+        movementScript.isBusy = true;
+        animator.SetTrigger("Attack3");
     }
 
     public void OnAttackEnd()
