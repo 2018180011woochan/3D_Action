e16945afcfa70a44a85800a116bd638a8f015394
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-08-12 18:35:30 +0900

    mutant 페이즈 전환

--- Assets/00_Script/Boss/Mutant/MutantAI.cs
+++ Assets/00_Script/Boss/Mutant/MutantAI.cs
@@ -1,16 +1,50 @@
+using System;
 using UnityEngine;
 
+public enum MutantPhase { Phase1, Phase2 }
 public class MutantAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private MonsterState monsterState;
+    public MonoBehaviour phase1Controller;
+    public MonoBehaviour phase2Controller;
+    public float phaseSwapHP = 50f;
+
+    public MutantPhase CurrentPhase { get; private set; } = MutantPhase.Phase1;
+    public Action<MutantPhase> OnPhaseChanged;
+
+    void Awake()
     {
-        
+        monsterState = GetComponent<MonsterState>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        EvaluatePhase(true);
+    }
+
     void Update()
     {
-        
+        EvaluatePhase();
+    }
+
+
+    private void EvaluatePhase(bool force = false)
+    {
+        float hp = monsterState.currentHP;
+        MutantPhase next = (hp < phaseSwapHP) ? MutantPhase.Phase2 : MutantPhase.Phase1;
+
+        if (force || next != CurrentPhase)
+        {
+            CurrentPhase = next;
+            ApplyPhaseControllers(CurrentPhase);
+            OnPhaseChanged?.Invoke(CurrentPhase);
+            Debug.Log($"[MutantAI] Phase ¡æ {CurrentPhase}");
+        }
+    }
+
+    private void ApplyPhaseControllers(MutantPhase phase)
+    {
+        if (phase1Controller) phase1Controller.enabled = (phase == MutantPhase.Phase1);
+        if (phase2Controller) phase2Controller.enabled = (phase == MutantPhase.Phase2);
     }
 }
